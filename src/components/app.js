1f01e6a15208037e4760b96c2e0e63bd516e55c3
Gaurav Kelkar <gauravkelkar35@gmail.com>
2022-11-16 15:15:40 +0530

    Added react page

---
+++ src/components/app.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+const App = () => {
+	return (
+		<React.Fragment>
+			<h1>Hello from gaurav</h1>
+		</React.Fragment>
+	);
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+	//specify which file to place at the top of the render DOM tree
+	entry: "./src/index.js",
+
+	//specify to create bundle.js file at path /dist/bundle.js
+	output: {
+		path: path.join(__dirname, "/dist"),
+		filename: "bundle.js",
+	},
+
+	//specify that the webpack render index.html with bundle.js at the top of the render DOM tree
+	plugins: [
+		new HTMLWebpackPlugin({
+			template: path.join(__dirname, "/src/index.html"),
+		}),
+	],
+
+	//transpiler loader specs
+	module: {
+		rules: [
+			{
+				//transpile files ending with .js
+				test: /.js$/,
+
+				// node_modules from transpilation
+				exclude: /node_modules/,
+				use: {
+					// use the babel-loader package to transpile
+					loader: "babel-loader",
+					options: {
+						//use these presets
+						presets: ["@babel/preset-env", "@babel/preset-react"],
+					},
+				},
+			},
+		],
+	},
+};
